585a8ce1b6c80174e4bd355b5d83988da2770882
Jess Belliveau <jess.belliveau@gmail.com>
2013-07-08 15:46:05 +1000

    Progress. FAQ page. Corrected forms

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -44,7 +44,7 @@ class PartsController < ApplicationController
 
     respond_to do |format|
       if @part.save
-        format.html { redirect_to all_path, notice: 'Part was successfully created.' }
+        format.html { redirect_to :back, notice: 'Part was successfully created.' }
         format.json { render json: @part, status: :created, location: @part }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class PartsController < ApplicationController
 
     respond_to do |format|
       if @part.update_attributes(params[:part])
-        format.html { redirect_to all_path, notice: 'Part was successfully updated.' }
+        format.html { redirect_to project_path(@part.project.name), notice: 'Part was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
